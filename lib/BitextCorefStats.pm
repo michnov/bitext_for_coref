437d7ab5be7f1c2609c4f36de8b22fd2d5b49c03
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2013-11-13 12:32:25 +0000

    to generate the 'svuj_en_counterpart' stats and 'print_cs_relpron_en_partic' stats; probably inconsistent status
    Fri Oct 25 08:57:44 CEST 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@11216 7cf035df-8472-40d0-8fbe-a733a24d2855

--- lib/BitextCorefStats.pm
+++ lib/BitextCorefStats.pm
@@ -5,26 +5,95 @@ use utf8;
 use Moose;
 use Treex::Core::Common;
 use Treex::Tool::Align::Utils;
+use Readonly;
+use Array::Utils qw/intersect/;
 
-extends 'Treex::Core::Block';
+use Data::Dumper;
+
+extends 'Treex::Block::Write::BaseTextWriter';
+
+Readonly::Hash my %EN_SRC_FILTER => (language => 'en', selector => 'src');
+Readonly::Hash my %CS_REF_FILTER => (language => 'cs', selector => 'ref');
+Readonly::Hash my %EN_REF_FILTER => (language => 'en', selector => 'ref');
 
 sub process_tnode {
     my ($self, $tnode) = @_;
 
-    print_svuj_english_counterpart($tnode);
+    my $err_msg;
+    
+    $err_msg = $self->print_svuj_en_counterpart($tnode);
+    log_info $tnode->get_address . "\t" . $err_msg if (defined $err_msg);
+    
+    $err_msg = $self->print_cs_relpron_en_partic($tnode);
+    log_info $tnode->get_address . "\t" . $err_msg if (defined $err_msg);
 }
 
-sub print_svuj_english_counterpart {
-    my ($tnode) = @_;
+sub print_svuj_en_counterpart {
+    my ($self, $tnode) = @_;
 
     my $anode = $tnode->get_lex_anode();
     return if (!defined $anode || $anode->lemma !~ /^svůj/);
 
-    my @en_src = Treex::Tool::Align::Utils::aligned_transitively([$tnode], [{language => 'en', selector => 'src'}]);
-    print join ",", (map {$_->t_lemma} @en_src);
-    print "\t";
-    print "(" . (join ", ", map {$_->get_address} @en_src) . ")";
-    print "\n";
+    my @en_src = Treex::Tool::Align::Utils::aligned_transitively([$tnode], [\%EN_SRC_FILTER]);
+    print {$self->_file_handle} "svuj_en_counterpart\t";
+    print {$self->_file_handle} join ",", (map {$_->t_lemma} @en_src);
+    print {$self->_file_handle} "\t";
+    print {$self->_file_handle} "(" . (join ", ", map {$_->get_address} @en_src) . ")";
+    print {$self->_file_handle} "\n";
+}
+
+sub print_cs_relpron_stats {
+    my ($self, $tnode) = @_;
+    
+    # searching for Czech relative pronouns
+    my $indeftype = $tnode->gram_indeftype;
+    return if (!defined $indeftype || $indeftype ne "relat");
+
+    $self->print_cs_relpron_prf($tnode);
+}
+
+sub print_cs_relpron_prf {
+    my ($self, $tnode) = @_;
+
+    my @cs_antes = $tnode->get_coref_gram_nodes;
+
+}
+
+sub print_cs_relpron_en_partic {
+    my ($self, $tnode) = @_;
+
+    # searching for Czhech relative pronouns
+    my $indeftype = $tnode->gram_indeftype;
+    return if (!defined $indeftype || $indeftype ne "relat");
+
+    # must not be aligned to anything
+    # TODO: or aligned to something not relevant
+    my @en_src = Treex::Tool::Align::Utils::aligned_transitively([$tnode], [\%EN_SRC_FILTER]);
+    return "relpron aligned with: " . (join ",", map {$_->t_lemma} @en_src) if (@en_src);
+
+    # an English counterpart of the Czech verb must be a past participle or a gerund
+    my ($cs_verb_par) = grep {$_->formeme =~ /^v/} $tnode->get_eparents();
+    return "cs parent is not a verb" if (!defined $cs_verb_par);
+    my ($par_en_src) = Treex::Tool::Align::Utils::aligned_transitively([$cs_verb_par], [\%EN_SRC_FILTER]);
+    return "no en node aligned with a cs parent" if (!defined $par_en_src);
+    my $par_en_src_alex = $par_en_src->get_lex_anode;
+    return "en par-align undefined or not a participle" if (!defined $par_en_src_alex || ($par_en_src_alex->tag ne 'VBG' && $par_en_src_alex->tag ne 'VBN'));
+
+    # possible antecedents via relpron->participle projection
+    my @en_rel_partic_antes = $par_en_src->get_eparents;
+
+    # possible antecedents via alignment with the Czech estimated antecedent
+    my @cs_antes = $tnode->get_coref_gram_nodes;
+    my @en_antes = Treex::Tool::Align::Utils::aligned_transitively(\@cs_antes, [\%EN_SRC_FILTER]);
+
+    my @en_antes_both = intersect(@en_rel_partic_antes, @en_antes);
+    
+
+    print {$self->_file_handle} "cs_relpron_en_partic\t";
+    print {$self->_file_handle} join ",", (map {$_->t_lemma} @en_rel_partic_antes);
+    print {$self->_file_handle} "\t";
+    print {$self->_file_handle} "(" . (join ", ", map {$_->get_address} @en_rel_partic_antes) . ")";
+    print {$self->_file_handle} "\n";
 }
 
 
