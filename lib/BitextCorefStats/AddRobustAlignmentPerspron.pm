afbcef49f51d75e689501a40c6afefa0e53dc099
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2014-04-03 13:08:15 +0000

    a special block for robust alignment + ordering of filtered aligned nodes
    - if a link already exists, "robust" is added to types
    - otherwise it creates a new link
    - one can determine a preference (using a regexp) for types of aligned nodes
        - e.g. rel_types => ["robust", "monolingual", ".*"]
          returns the list of aligned nodes in determined ordering (".*" meaning the rest)
    
    Thu Nov 21 00:54:50 CET 2013
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@12160 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/BitextCorefStats/AddRobustAlignmentPerspron.pm
@@ -0,0 +1,118 @@
+package Treex::Block::My::BitextCorefStats::AddRobustAlignmentPerspron;
+
+use Moose;
+use Treex::Tool::Align::Utils;
+use Treex::Core::Common;
+
+use Treex::Block::My::BitextCorefStats;
+
+extends 'Treex::Core::Block';
+
+has 'align_lang' => (is => 'ro', isa => 'Treex::Type::LangCode', required => 1);
+has 'align_selector' => (is => 'ro', isa => 'Treex::Type::Selector', required => 1);
+
+has '_align_zone' => (is => 'ro', isa => 'HashRef[Str]', builder => '_build_align_zone', lazy => 1);
+
+has 'type' => (is => 'ro', isa => 'Str', default => 'robust');
+
+sub BUILD {
+    my ($self) = @_;
+    $self->_align_zone;
+}
+
+sub _build_align_zone {
+    my ($self) = @_;
+    return {language => $self->align_lang, selector => $self->align_selector};
+}
+
+sub process_tnode {
+    my ($self, $tnode) = @_;
+
+    # only for 3rd person non-reflexive personal pronouns
+    return if (!is_perspron($tnode, 0, 1));
+    
+    my $sieves = [ 'self', 'eparents', 'siblings', \&access_via_ancestor ];
+    my $filters = [ \&filter_self, \&filter_eparents, \&filter_siblings, \&filter_ancestor ];
+
+    my ($result_node, $errors) = Treex::Tool::Align::Utils::aligned_robust($tnode, [ $self->_align_zone ], $sieves, $filters);
+    if (defined $result_node) {
+        Treex::Tool::Align::Utils::add_aligned($tnode, $result_node, $self->type);
+    }
+}
+
+sub is_perspron {
+    my ($tnode, $is_reflex, $is_3rd_person) = @_;
+    my $tnode_3rd_person = defined $tnode->gram_person && ($tnode->gram_person eq "3");
+    return 
+        ($tnode->t_lemma eq "#PersPron") &&
+        ($tnode->get_attr('is_reflexive') xor !$is_reflex) &&
+        ($tnode_3rd_person xor !$is_3rd_person)
+            ? 1 : 0;
+}
+
+
+sub access_via_ancestor {
+    my ($tnode, $align_filters, $errors) = @_;
+
+    my $verb_par = $tnode->get_parent();
+    while (defined $verb_par && (!defined $verb_par->formeme || $verb_par->formeme !~ /^v/)) {
+        $verb_par = $verb_par->get_parent();
+    }
+    if (!defined $verb_par) {
+        push @$errors, "NO_EN_REF_VERB_ANCESTOR";
+        return;
+    }
+
+    my @aligned_verb_par = Treex::Tool::Align::Utils::aligned_transitively([$verb_par], $align_filters);
+    if (!@aligned_verb_par) {
+        push @$errors, "NO_CS_REF_VERB_PAR";
+        return;
+    }
+    return @aligned_verb_par;
+}
+
+sub filter_self {
+    my ($aligned, $tnode, $errors) = @_;
+
+    my $aligned_first = shift @$aligned;
+    my $anode = $aligned_first->get_lex_anode();
+    if (!defined $anode || ($anode->tag !~ /^P[8SDP5]/)) {
+        push @$errors, "NOPRON_CS_REF_TNODE";
+        return;
+    }
+    return $aligned_first;
+}
+
+sub filter_eparents {
+    my ($aligned, $tnode, $errors) = @_;
+    my $filtered = Treex::Block::My::BitextCorefStats::filter_by_functor($aligned, $tnode->functor, $errors);
+    return $filtered;
+}
+
+sub filter_siblings {
+    my ($aligned, $tnode, $errors) = @_;
+    my $par = Treex::Block::My::BitextCorefStats::eparents_of_aligned_siblings($aligned, $errors);
+    return if (!$par);
+    my @kids = $par->get_echildren({or_topological => 1});
+    my $filtered = Treex::Block::My::BitextCorefStats::filter_by_functor(\@kids, $tnode->functor, $errors);
+    return $filtered;
+}
+
+sub filter_ancestor {
+    my ($aligned, $tnode, $errors) = @_;
+
+    my $aligned_first = shift @$aligned;
+    my ($aligned_dative_child) = grep {
+        my $anode = $_->get_lex_anode; 
+        if (defined $anode) {$anode->tag =~ /^P...3/}
+    } $aligned_first->get_echildren({or_topological => 1});
+    
+    if (!defined $aligned_dative_child) {
+        push @$errors, "NO_CS_REF_DATIVE_CHILD";
+        return;
+    }
+    push @$errors, "BENEF_FOUND";
+    return $aligned_dative_child;
+}
+
+1;
