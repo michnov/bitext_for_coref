fb29817fb799d177b1b20efcd08ae2e5fddd72ad
mnovak <mnovak@7cf035df-8472-40d0-8fbe-a733a24d2855>
2015-04-30 09:07:25 +0000

    removing loops
    Thu Mar 12 11:18:44 CET 2015
    
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/tectomt_devel/trunk@14867 7cf035df-8472-40d0-8fbe-a733a24d2855

---
+++ lib/RemoveCoreferenceLoops.pm
@@ -0,0 +1,19 @@
+package Treex::Block::My::RemoveCoreferenceLoops;
+
+use Moose;
+use Treex::Core::Common;
+
+use Treex::Tool::Align::Utils;
+
+extends 'Treex::Core::Block';
+
+sub process_tnode {
+    my ($self, $tnode) = @_;
+
+    my @antes = $tnode->get_coref_nodes();
+    my @looped = grep {$_ == $tnode} @antes;
+
+    $tnode->remove_coref_nodes(@looped);
+}
+
+1;
